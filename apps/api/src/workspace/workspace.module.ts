7559d911e0243b7a101557000f7425a23785da5f
jdes01 <jasampa3000@gmail.com>
2023-03-17 19:14:12 +0100

    feat: import database module

--- apps/api/src/workspace/workspace.module.ts
+++ apps/api/src/workspace/workspace.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
+import { EventSourcingModule } from 'event-sourcing-nestjs';
+import { DatabaseModule } from '../mongodb/mongodb.module';
 
 import { CommandHandlers } from './application/command'
 import { WorkspaceProviders } from './workspace.providers';
@@ -9,7 +11,7 @@ const Sagas = [];
 
 @Module({
     imports: [
-        CqrsModule,
+        CqrsModule, DatabaseModule, EventSourcingModule
     ],
     providers: [...CommandHandlers, ...QueryHandlers, ...Sagas, ...WorkspaceProviders],
 })
--- apps/api/src/workspace/workspace.providers.ts
+++ apps/api/src/workspace/workspace.providers.ts
@@ -2,8 +2,7 @@ import { WORKSPACE_REPOSITORY } from './domain/repository';
 import { WorkspaceEventStoreRepository } from './infrastructure/repository';
 
 import { WORKSPACE_PROJECTION } from './domain/projection/';
-import { WorkspaceSchema } from './infrastructure/projection/workspace.schema';
-import { Connection } from 'mongoose';
+import { WorkspaceMongoDBProjection } from './infrastructure/projection/workspace.projection';
 
 export const WorkspaceProviders = [
     {
@@ -12,7 +11,6 @@ export const WorkspaceProviders = [
     },
     {
         provide: WORKSPACE_PROJECTION,
-        useFactory: (connection: Connection) => connection.model('Workspace', WorkspaceSchema),
-        inject: ['DATABASE_CONNECTION']
+        useClass: WorkspaceMongoDBProjection
     },
 ];
